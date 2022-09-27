eeafef609cb8f2c8f43d828f86651bb59a280925
tareita <tareitaa@outlook.com>
2022-09-27 21:47:22 +0100

    1 not a prime number

--- primes.py
+++ primes.py
@@ -5,7 +5,7 @@ def primes(number_of_primes):
     if number_of_primes <= 0:
         raise ValueError("must be positive")
     list = []
-    i = 1
+    i = 2
     while len(list) < number_of_primes:
         is_prime = True 
         for j in range(2, i-1):
