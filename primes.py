224868f1528703de515a6f8ea84ed9de67dc4706
tareita <tareitaa@outlook.com>
2022-09-27 21:46:00 +0100

    primes

--- primes.py
+++ primes.py
@@ -2,5 +2,16 @@
 """ENTER YOUR SOLUTION HERE!"""
 
 def primes(number_of_primes):
+    if number_of_primes <= 0:
+        raise ValueError("must be positive")
     list = []
+    i = 1
+    while len(list) < number_of_primes:
+        is_prime = True 
+        for j in range(2, i-1):
+            if i / j == i // j:
+                is_prime = False
+        if is_prime:
+            list.append(i)
+        i += 1
     return list
